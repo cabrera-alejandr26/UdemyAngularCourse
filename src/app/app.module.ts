47b0fe1ee02f1ce5b37780181e171f8270cb7bf2
cabrera-alejandr26 <cabrera.alejandr26@gmail.com>
2021-10-01 15:06:14 -0700

    fixed git ignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';//Manually import the server component, it needs to be done in order for TypeScript can recognize where the server component is located.
+import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
+//Angular
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent, //TypeScript needs to find where to find this server component.
+    WarningComponent, SuccessComponent,
   ],
   imports: [
     BrowserModule
